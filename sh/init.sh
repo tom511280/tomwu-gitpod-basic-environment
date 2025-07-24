b6bd4af18c1871ba274648f5fa6672adc1368a70
tom511280 tom511280 <tom511280@gmail.com>
2025-07-24 07:49:28 +0000

    feat : Update initialization executable file

--- sh/init.sh
+++ sh/init.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+echo "✅ init.sh triggered" >> /workspace/init.log
+
+
 # 定義變量
 PORTAINER_COMPOSE_PATH="/workspace/tomwu-gitpod-basic-environment/portainer/docker-compose.yaml"
 
@@ -10,3 +14,5 @@ docker-compose -f "$PORTAINER_COMPOSE_PATH" up -d
 # 等待一段時間讓伺服器啟動完成
 echo "Wait 15 seconds..."
 sleep 15
+
+echo "[$(date)] init.sh 執行完成" >> /workspace/init.log
